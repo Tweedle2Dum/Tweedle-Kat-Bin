4d8cc624043f21155bc9dd866169af63291e35b7
Tweedle2DUm <utkarsh.work1405@gmail.com>
2022-10-05 23:26:52 +0530

    stuff

---
+++ frontend/pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function About(){
+  return (
+    <div>This is about page</div>
+  )
+}
+
+export default About
