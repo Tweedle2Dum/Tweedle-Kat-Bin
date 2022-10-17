5a3ec1b4b02335429686a383adc1b7040c1a7278
Tweedle2DUm <utkarsh.work1405@gmail.com>
2022-10-18 03:05:39 +0530

    added. useState for input

--- frontend/components/TextArea.tsx
+++ frontend/components/TextArea.tsx
@@ -1,11 +1,18 @@
+import { detectContentType } from "next/dist/server/image-optimizer";
 import React from "react";
+import { useState } from "react";
 import styles from "../styles/Pastebin.module.css"
 
 
 export default function TextArea() {
+    const [text,setText]=useState('');
+
+    function handleChange(e){
+        setText(e.target.value);
+    }
     return (
         <div className="text-container">
-            <textarea className="textarea" placeholder="Enter your ideas here!!!"></textarea>
+            <textarea  name="content" className="textarea" onChange={handleChange} placeholder="Enter your ideas here!!!"></textarea>
         </div>
 
     );
