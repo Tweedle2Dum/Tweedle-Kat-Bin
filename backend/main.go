6548107622ed69741a1f5ec737540abc3a89a3d1
Lioncat2002 <sankhayan2002@gmail.com>
2022-10-28 13:09:38 +0530

    feat: backend done

--- backend/main.go
+++ backend/main.go
@@ -1,78 +1,86 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
 	"os"
-
-	"github.com/cockroachdb/cockroach-go/v2/crdb/crdbgorm"
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 )
 
 type Entry struct {
-	Id  uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Id  uint64 `gorm:"primary_key;auto_increment;not_null"`
 	Txt string `json:"txt"`
 }
 
-
-var ids []uuid.UUID
-
-func addData()
-
-func data(c *gin.Context) {
-	var entry Entry
-	dotenv := os.Getenv("DB_URL")
-
-	if c.Request.Method == "POST" {
-		if err := c.BindJSON(&entry); err != nil {
-			c.IndentedJSON(http.StatusCreated,gin.H{
-        "error":err,
-      })
+func data(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Method == "POST" {
+			var entry Entry
+			if err := c.BindJSON(&entry); err != nil {
+				c.IndentedJSON(http.StatusCreated, gin.H{
+					"error": err,
+				})
+			}
+			err := db.Create(&entry).Error
+			if err != nil {
+				c.IndentedJSON(http.StatusCreated, gin.H{
+					"error": err,
+				})
+			}
+			c.IndentedJSON(http.StatusCreated, gin.H{
+				"entry": entry,
+			})
 		}
-		c.IndentedJSON(http.StatusCreated, entry)
+
+		if c.Request.Method == "GET" {
+			var entries []Entry
+			db.Find(&entries)
+			c.IndentedJSON(http.StatusOK, gin.H{
+				"result": entries,
+			})
+		}
 	}
 
-	if c.Request.Method == "GET" {
-		c.IndentedJSON(http.StatusOK, entries)
-	}
 }
 
-func collection(c *gin.Context){
-	c.IndentedJSON(http.StatusOK,entries)
-
-
-}
 func ping(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
 }
 func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
+	return func(c *gin.Context) {
 
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
 
 func main() {
 	godotenv.Load()
+	dburi := os.Getenv("DB_URL")
+	db, err := gorm.Open(postgres.Open(dburi), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	}
+	db.AutoMigrate(&Entry{})
+
 	r := gin.Default()
 	r.Use(CORSMiddleware())
 	r.GET("/", ping)
-	r.GET("data/", data)
-	r.POST("data/", data)
-	r.GET("collection/",collection)
+	r.GET("data/", data(db))
+	r.POST("data/", data(db))
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
